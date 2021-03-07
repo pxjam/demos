b09f52dd691a48fb0aea230a013f34a85f7cc8cd
d.udovichenko <d.udovichenko@gmail.com>
2021-03-07 23:01:56 +0300

    gltf sputnik fixes

--- 3d-gltf/index.js
+++ 3d-gltf/index.js
@@ -1,16 +1,19 @@
 import * as THREE from 'three'
+import sputnik from './models/sputnik/sputnik.gltf'
 
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
 // import {KMZLoader} from 'three/examples/jsm/loaders/KMZLoader.js'
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js'
 
-let camera, scene, renderer
+let scene, renderer
+
+window.camera = null
 
 init()
 
 function init() {
     scene = new THREE.Scene()
-    scene.background = new THREE.Color(0x999999)
+    scene.background = new THREE.Color(0x332288)
 
     const light = new THREE.DirectionalLight(0xffffff)
     light.position.set(0.5, 1.0, 0.5).normalize()
@@ -19,13 +22,14 @@ function init() {
 
     camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 500)
 
-    camera.position.y = 5
+    camera.position.x = 10
+    camera.position.y = 10
     camera.position.z = 10
 
     scene.add(camera)
 
-    const grid = new THREE.GridHelper(50, 50, 0xffffff, 0x555555)
-    scene.add(grid)
+    // const grid = new THREE.GridHelper(50, 50, 0xbbbbbb, 0x777777)
+    // scene.add(grid)
 
     renderer = new THREE.WebGLRenderer({antialias: true})
     renderer.setPixelRatio(window.devicePixelRatio)
@@ -33,19 +37,19 @@ function init() {
     document.body.appendChild(renderer.domElement)
 
     const loader = new GLTFLoader()
-    // loader.setPath('models/helmet/')
-    loader.setPath('models/sputnik/')
-    loader.load('./sputnik.gltf', function(gltf) {
-        // loader.load('./DamagedHelmet.gltf', function(gltf) {
-        //gltf.scene.position.y = 0.5
-        // gltf.scene.scale(0.1)
-        scene.add(gltf.scene)
 
+    console.log(sputnik)
+    
+    // loader.setPath('models/sputnik/')
+    loader.load(sputnik, function(gltf) {
+        // loader.load('./DamagedHelmet.gltf', function(gltf) {
+        gltf.scene.position.y = 1
         gltf.scene.scale.x = .03
         gltf.scene.scale.y = .03
         gltf.scene.scale.z = .03
 
-        console.log(gltf.scene)
+        scene.add(gltf.scene)
+
         render()
     })
 
