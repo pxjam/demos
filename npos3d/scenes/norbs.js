9379dcda35a94cf367381d728410bc6491d722fa
d.udovichenko <d.udovichenko@gmail.com>
2021-03-31 00:45:33 +0300

    rebuild npos shell

--- npos3d/scenes/norbs.js
+++ npos3d/scenes/norbs.js
@@ -1,29 +1,56 @@
+import Tweakpane from 'tweakpane'
+import NPos3d from '../modules/npos3d'
+import {paneOptions} from '../modules/paneOptions'
+import mouse from '../modules/mouse'
+import presets from '../presets/shell'
+
+let paramsDefault = {
+    size: 10,
+    count: 12
+}
+let params = Object.assign({}, paramsDefault)
+
+window.pane = new Tweakpane({
+    container: document.querySelector('[data-pane]')
+})
+let f1 = pane.addFolder({
+    title: 'Настройки',
+    expanded: false
+})
+
+f1.addInput(params, 'size', {min: 1, max: 100, step: 1})
+f1.addInput(params, 'count', {min: 1, max: 100, step: 1})
+
+
 //Please excuse the fact that I do not declare the variables which I would like to have accessible globally
-n = NPos3d
-s = new n.Scene()
+let lib = NPos3d
+let scene = new lib.Scene({
+    canvas: document.querySelector('[data-canvas]'),
+    backgroundColor: '#fff'
+})
 
 let Norb = function(args) {
-    var t = this, type = 'Norb'
+    let t = this, type = 'Norb'
     if (t.type !== type) {
         throw type + ' constructor requires the use of the `new` keyword.'
     }
     args = args || {}
     t.offset = args.offset
-    n.blessWith3DBase(t, args)
-    s.add(t)
+    lib.blessWith3DBase(t, args)
+    scene.add(t)
 }
 
 Norb.prototype = {
     type: 'Norb',
-    shape: n.Geom.cube,
+    shape: lib.Geom.cube,
     update: function() {
-        var t = this, fac
+        let t = this, fac
         t.rot[0] += deg
         t.rot[1] += deg
-        t.pos[0] = cos((tau / divisor * t.offset) + t.rot[1]) * 250
-        t.pos[1] = sin((tau / divisor * t.offset) + t.rot[1]) * 250
+        t.pos[0] = cos((tau / params.count * t.offset) + t.rot[1]) * 250
+        t.pos[1] = sin((tau / params.count * t.offset) + t.rot[1]) * 250
 
-        fac = 1 + Math.sin(tau / divisor * (t.offset + (time / 100)))
+        fac = params.size + Math.sin(tau / params.count * (t.offset + (time / 100)))
 
         t.scale[0] = t.scale[1] = t.scale[2] = fac
     }
@@ -36,16 +63,28 @@ let timeUpdater = {
         let time = date.getTime()
     }
 }
-s.add(timeUpdater)
+scene.add(timeUpdater)
+
 let myNorbs = []
-let divisor = 20
 let spacing = 40
 
-for (let i = 0; i < divisor; i += 1) {
+function fixNorbsCount() {
+    if (myNorbs.length !== params.count) {
+        let diff = params.count - myNorbs.length
+
+        if (diff > 0) {
+
+        } else if (diff < 0) {
+
+        }
+    }
+}
+
+for (let i = 0; i < params.count; i++) {
     myNorbs.push(new Norb({
-        pos: [(spacing * i) - (((divisor - 1) * spacing) / 2), 0, 0],
-        color: 'hsl(' + ((360 / divisor) * i) + ',100%,50%)',
-        scale: [500, 5, 5],
+        pos: [(spacing * i) - (((params.count - 1) * spacing) / 2), 0, 0],
+        color: '#09f',
+        scale: [5, 5, 5],
         offset: i
     }))
 }
