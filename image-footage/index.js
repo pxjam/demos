2c966809eadd60a93e997a0c0e8f5c216c4ade61
Dmitry Orlov <dmitryorly611@gmail.com>
2022-06-28 11:58:06 +0300

    add image-footage demo

---
+++ image-footage/index.js
@@ -0,0 +1,80 @@
+const { magneticConstantDependencies } = require("mathjs/lib/cjs/entry/dependenciesAny.generated")
+
+function imageFromSrc(src, alt = '') {
+    const image = new Image()
+    image.src = src
+    image.alt = alt
+    return image
+}
+
+/**
+ * @param {HTMLElement} trigger 
+ * @param {HTMLImageElement[]} images
+ * @param {number} delay
+ */
+function imageFootage(trigger, images, delay) {
+    let currentIndex = 0
+    let timerId = null
+    let shouldStop = false
+
+    const getNextIndex = () => (currentIndex + 1) % images.length
+    const hide = (index) => images[index].style.opacity = '0'
+    const show = (index) => images[index].style.opacity = '1'
+
+    images.forEach((_, i) => hide(i))
+    show(currentIndex)
+
+    const reset = () => {
+        hide(currentIndex)
+        show(0)
+        currentIndex = 0
+        shouldStop = false
+        timerId = null
+    }
+
+    const loop = () => {
+        if (shouldStop) {
+            reset()
+            return
+        }
+
+        timerId = setTimeout(loop, delay)
+
+        const nextIndex = getNextIndex()
+
+        hide(currentIndex)
+        show(nextIndex)
+
+        currentIndex = nextIndex
+    }
+
+    const start = () => {
+        if (timerId) return
+        loop()
+    }
+
+    const stop = () => {
+        if (!timerId) return
+        shouldStop = true
+    }
+
+    trigger.addEventListener('mouseenter', start)
+    trigger.addEventListener('mouseleave', stop)
+}
+
+const baseUrl = 'https://s3.timeweb.com/ch23840-sozdanie-dev/template/img/'
+const imageNames = [
+    'stroitelstvo.jpg',
+    'project-1.jpg',
+    'project-2.jpg',
+    'cover-1.jpg',
+    'cover-2.jpg',
+    'building.jpg',
+    'block.jpg'
+]
+const images = imageNames.map((name, i) => imageFromSrc(baseUrl + name, `image #${i}`))
+
+const root = document.querySelector('[data-image-footage]')
+images.forEach((img) => root.append(img))
+
+imageFootage(root, images, 1000 / 5)
