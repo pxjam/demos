6c21593f98a58a299ef049f186a053c360b5f97b
Dmitry Orlov <dmitryorly611@gmail.com>
2022-07-01 11:30:09 +0300

    add gsap to inner-parallax demo

---
+++ inner-parallax/gsap.js
@@ -0,0 +1,25 @@
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
+
+gsap.utils.toArray("[data-inner-parallax]").forEach((container, i) => {
+    container.bg = container.querySelector("[data-inner-parallax-bg]")
+
+    // the first image (i === 0) should be handled differently because it should start at the very top.
+    // use function-based values in order to keep things responsive
+    gsap.fromTo(container.bg, {
+        y: () => -container.bg.offsetHeight / 4
+    }, {
+        y: () => container.bg.offsetHeight / 4,
+        ease: "none",
+        scrollTrigger: {
+            trigger: container,
+            start: () => "top bottom",
+            end: "bottom top",
+            scrub: true,
+            invalidateOnRefresh: true // to make it responsive
+        }
+    });
+
+});
